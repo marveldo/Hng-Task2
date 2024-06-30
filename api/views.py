ab21a2ae9a0f03f3f307a102b92b03e650741ba2
utibe solomon <utibesolomon6@gmail.com>
2024-07-01 00:29:22 +0100

    added changes

--- api/views.py
+++ api/views.py
@@ -4,15 +4,26 @@ from rest_framework import renderers
 from rest_framework.response import Response
 import requests
 import json
+import geocoder
 from rest_framework import status
+from ipware import get_client_ip
 # Create your views here.
 
 
 class AboutUserview(APIView):
     renderer_classes = [renderers.JSONRenderer]
-
     
-    
+    def get_ip(self,request):
+        ip_address = request.META.get('HTTP_X_FORWARDED_FOR')
+        if not ip_address:
+           ip_address = request.META.get('REMOTE_ADDR')
+        return ip_address
+    def get_city(self, ip):
+       
+        location = geocoder.ip(ip)
+        city = location.city
+        print(city)
+        return city
        
     
     def get_weather(self,city) :
@@ -25,12 +36,13 @@ class AboutUserview(APIView):
         return f"{(farenheit - 32) * 5.0/9.0 : .2f}"
     
     def get(self, request, *args, **kwargs):
+
         if self.request.GET.get('visitor_name'):
            name = self.request.GET.get('visitor_name')
-           ip = json.loads(requests.get('https://api.ipify.org?format=json').text)
-           city = json.loads(requests.get(f'http://ip-api.com/json/{ip["ip"]}').text)['city']
+           ip = self.get_ip(request)
+           city = self.get_city(ip)
            temp = self.get_weather(city)
-           res = {'client_ip' : ip["ip"] ,
+           res = {'client_ip' : ip,
                'location' : city,
                'greeting': f'hello {name}! the temperature is {self.convert_to_celsius(temp)} in {city}'
         
